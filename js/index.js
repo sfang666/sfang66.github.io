b4eff5ab60e74d777f10f7d71c4870e92d55cb41
sfang666 <38084158+sfang666@users.noreply.github.com>
2018-04-04 14:24:44 -0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -15,14 +15,14 @@ function onLeaveHeadLine(hl){
 	hl.style.textShadow = "";
 }
 
-function sleep(numberMillis) { 
-	var now = new Date(); 
-	var exitTime = now.getTime() + numberMillis; 
-	while (true) { 
-		now = new Date(); 
-		if (now.getTime() > exitTime) 
-		return; 
-	} 
+function sleep(numberMillis) {
+	var now = new Date();
+	var exitTime = now.getTime() + numberMillis;
+	while (true) {
+		now = new Date();
+		if (now.getTime() > exitTime)
+		return;
+	}
 }
 
 function onTouchPicture(pic){
@@ -34,7 +34,7 @@ function onTouchPicture(pic){
 }
 
 function onLeavePicture(pic){
-	
+
 	console.log("abc");
 	pic.style.borderRadius = "50%";
 }
